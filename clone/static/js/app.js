9175e70b983dd94a0a49b805e6285e3ec80b05a9
Guna <gunagang24@gmail.com>
2021-07-17 12:26:40 +0530

    Front end finished bugs: chart size is not responsive

--- clone/static/js/app.js
+++ clone/static/js/app.js
@@ -5,13 +5,7 @@ const icomponents = document.querySelector('.icomponents');
 const cContainer = document.querySelector('.cc-container');
 const uContainer = document.querySelector('.uc-container');
 const iArrowRight = document.querySelector('.i-angle-right');
-const dots = document.querySelector('.dots3');
-const eOverview = document.querySelector('.e-overview');
-const dots1 = document.querySelector('.dots4');
-const rOverview = document.querySelector('.r-overview');
 
-var dotClicks1 = 0;
-var dotClicks = 0;
 var iArwCount = 0;
 var arwCount = 0;
 
@@ -52,7 +46,103 @@ uContainer.addEventListener('click', (e)=>{
 	}
 });
 
+// toggle 
+
+const toggle = document.querySelector('.fa-angle-left');
+const tgleContainer = document.querySelector('.round-toggle');
+const logo = document.querySelector('.logo');
+const logoCont = document.querySelector('.logo-cont');
+const navbarSection = document.querySelector('.navbar-section');
+const cmpContainer = document.querySelectorAll('.components-cont');
+var tgleCount = 0;
+
+tgleContainer.addEventListener('click', (e)=>{
+	if(tgleCount%2==0) {
+		toggle.classList.add('rotate-180');
+		cArrowRight.style.display = 'none';
+		iArrowRight.style.display = 'none';
+		logoCont.style.marginLeft = '0';
+		navbarSection.style.minWidth = 'auto';
+		logo.style.display = 'none';
+		cmpContainer.forEach((cmp)=>{
+			cmp.classList.add('center')
+		});
+		tgleCount++;
+
+	} else {
+		toggle.classList.remove('rotate-180');
+		cArrowRight.style.display = 'block';
+		iArrowRight.style.display = 'block';
+		navbarSection.style.minWidth = '12rem';
+		logoCont.style.marginLeft = '1rem';
+		logo.style.display = 'block';
+		cmpContainer.forEach((cmp)=>{
+			cmp.classList.remove('center')
+		});
+		tgleCount--;
+	}
+});
+// resize 
+
+window.onresize = ()=>{
+	if (window.innerWidth < 1000){
+		toggle.classList.add('rotate-180');
+		cArrowRight.style.display = 'none';
+		iArrowRight.style.display = 'none';
+		logoCont.style.marginLeft = '0';
+		navbarSection.style.minWidth = 'auto';
+		logo.style.display = 'none';
+		cmpContainer.forEach((cmp)=>{
+			cmp.classList.add('center')
+		});
+		tgleCount++;
+	} else {
+		toggle.classList.remove('rotate-180');
+		cArrowRight.style.display = 'block';
+		iArrowRight.style.display = 'block';
+		navbarSection.style.minWidth = '12rem';
+		logoCont.style.marginLeft = '1rem';
+		logo.style.display = 'block';
+		cmpContainer.forEach((cmp)=>{
+			cmp.classList.remove('center')
+		});
+		tgleCount--;
+	}
+}
+
+if (window.innerWidth < 1000){
+	toggle.classList.add('rotate-180');
+	cArrowRight.style.display = 'none';
+	iArrowRight.style.display = 'none';
+	logoCont.style.marginLeft = '0';
+	navbarSection.style.minWidth = 'auto';
+	logo.style.display = 'none';
+	cmpContainer.forEach((cmp)=>{
+		cmp.classList.add('center')
+	});
+	tgleCount++;
+} else {
+	toggle.classList.remove('rotate-180');
+	cArrowRight.style.display = 'block';
+	iArrowRight.style.display = 'block';
+	navbarSection.style.minWidth = '12rem';
+	logoCont.style.marginLeft = '1rem';
+	logo.style.display = 'block';
+	cmpContainer.forEach((cmp)=>{
+		cmp.classList.remove('center')
+	});
+	tgleCount--;
+}
+
+
 // overview options
+const dots = document.querySelector('.dots3');
+const eOverview = document.querySelector('.e-overview');
+const dots1 = document.querySelector('.dots4');
+const rOverview = document.querySelector('.r-overview');
+
+var dotClicks1 = 0;
+var dotClicks = 0;
 
 dots.addEventListener('click', (e)=>{
 	if (dotClicks %2==0){
@@ -81,11 +171,3 @@ dots1.addEventListener('click', (e)=>{
 		dotClicks1--;
 	}
 });
-
-
-
-
-
-
-
-
