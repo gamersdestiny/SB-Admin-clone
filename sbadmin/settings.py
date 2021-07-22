a8af894b4c644f37f578c639c93a1a689ee81bc6
Guna <gunagang24@gmail.com>
2021-07-22 15:26:01 +0530

    debug true

--- sbadmin/settings.py
+++ sbadmin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('sbadminkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sbadmin2clone.herokuapp.com', ]
 
